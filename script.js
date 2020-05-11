eaa1aca25739d68615b1346d964d34543c3a2823
Patrick Johnson <pjohnson24@icloud.com>
2020-05-11 11:17:40 -0400

    update 2

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let randomDamage = () => {
     return Math.floor(Math.random() * 10) + 1;
-
+    
 }
 let chooseOption = (opt1, opt2) => {
     let randNum = Math.round(Math.random());
     return randNum === 0 ? opt1 : opt2;
 }
 
-let attackPlayer = function (health) {
+let attackPlayer = function(health) {
     return health - randomDamage;
 }
 
@@ -21,28 +21,32 @@ let logDeath = (winner, loser) => {
 }
 
 let isDead = (health) => {
-    return health <= 0;
+    return health  <= 0 ;
 }
 
 function fight(player1, player2, player1Health, player2Health) {
-
-    
 
     var attacker = chooseOption(player1, player2);
 
     if (attacker === player1) {
         player2Health === attackPlayer(player2Health);
-   
-
+      
     logHealth(player2, player2Health);
-    isDead(player2Health); 
+    isDead(player2Health);
 
- }
+    if (isDead === true) {
+        logDeath(player1, player2);
+    }
+}    
     else {
         player1Health === attackPlayer(player1Health);
         logHealth(player1, player1Health);
         isDead(player1Health);
+    
+    if (isDead === true) {
+        logDeath(player2, player1);
     }
-
 }
+}
+
 fight('Patrick Johnson', 'Alison Bourdon', 100, 100);
