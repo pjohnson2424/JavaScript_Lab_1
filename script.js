5a343e2a23af7e4684246051f6d5dd2bcf804d4f
Patrick Johnson <pjohnson24@icloud.com>
2020-05-11 10:49:27 -0400

    update2

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let randomDamage = () => {
     return Math.floor(Math.random() * 10) + 1;
-}
 
+}
 let chooseOption = (opt1, opt2) => {
-    let randNum = Math.floor(Math.random() * Math.floor(1) + 1);
+    let randNum = Math.round(Math.random());
     return randNum === 0 ? opt1 : opt2;
 }
 
-let attackPlayer = function(health) {
-    return (`${health}` - `${randomDamage}`);
+let attackPlayer = function (health) {
+    return health - randomDamage;
 }
 
 let logHealth = (player, health) => {
-    console.log(`${player} health:${health}`);
+    console.log(`${player} health: ${health}`);
 }
 
 
@@ -20,39 +20,29 @@ let logDeath = (winner, loser) => {
     console.log(`${winner} defeated ${loser}`);
 }
 
-
 let isDead = (health) => {
-    i++;
-    return (health <= 0);
+    return health <= 0;
 }
 
 function fight(player1, player2, player1Health, player2Health) {
-    while (i = 0) {
-    }
-    var attacker = (`${chooseOption}`);
-    chooseOption(player1, player2);
 
-    if (`${attacker}` === `${player1}`) {
-        player2Health = attackPlayer(player2Health);
+    
 
-    logHealth.call(player2, player2Health);
+    var attacker = chooseOption(player1, player2);
 
-    isDead.call(player2Health);
-    while (true) {
-        logDeath.call(player1, player2);
-        break;
-   }
+    if (attacker === player1) {
+        player2Health === attackPlayer(player2Health);
+   
+
+    logHealth(player2, player2Health);
+    isDead(player2Health); 
+
  }
     else {
-    player1Health = attackPlayer(player1Health);
-}
-    logHealth.call(player1, player1Health);
-
-    isDead.call(player1Health);
-    while (true) {
-        logDeath.call(player2, player1);
-        break;
+        player1Health === attackPlayer(player1Health);
+        logHealth(player1, player1Health);
+        isDead(player1Health);
     }
-}
 
-fight.call("Patrick Johnson", "Alison Bourdon", 10, 10);
+}
+fight('Patrick Johnson', 'Alison Bourdon', 100, 100);
