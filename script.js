340ed3ddb002da74274df600bce2cc24b2299b9c
Patrick Johnson <pjohnson24@icloud.com>
2020-05-11 21:15:00 -0400

    final commit

--- script.js
+++ script.js
@@ -1,52 +1,44 @@
-let randomDamage = () => {
-    return Math.floor(Math.random() * 10) + 1;
-    
-}
-let chooseOption = (opt1, opt2) => {
-    let randNum = Math.round(Math.random());
-    return randNum === 0 ? opt1 : opt2;
-}
+let randomDamage = Math.floor(Math.random() * 11);
+const chooseOption = (opt1, opt2) => {
+  let randNum = Math.round(Math.random());
+  return randNum === 0 ? opt1 : opt2;
+};
 
-let attackPlayer = function(health) {
-    return health - randomDamage;
-}
+let attackPlayer = function (health) {
+  return health - randomDamage;
+};
 
 let logHealth = (player, health) => {
-    console.log(`${player} health: ${health}`);
-}
-
+  console.log(`${player} health: ${health}`);
+};
 
 let logDeath = (winner, loser) => {
-    console.log(`${winner} defeated ${loser}`);
-}
+  console.log(`${winner} defeated ${loser}`);
+};
 
 let isDead = (health) => {
-    return health  <= 0 ;
-}
+  return health <= 0;
+};
 
 function fight(player1, player2, player1Health, player2Health) {
+  while (true) {
+    let attacker = chooseOption(player1, player2);
+    if (attacker == player1) {
+      player2Health = attackPlayer(player2Health);
 
-    var attacker = chooseOption(player1, player2);
-
-    if (attacker === player1) {
-        player2Health === attackPlayer(player2Health);
-      
-    logHealth(player2, player2Health);
-    isDead(player2Health);
-
-    if (isDead === true) {
+      logHealth(player2, player2Health);
+      if (isDead(player2Health)) {
         logDeath(player1, player2);
-    }
-}    
-    else {
-        player1Health === attackPlayer(player1Health);
-        logHealth(player1, player1Health);
-        isDead(player1Health);
-    
-    if (isDead === true) {
+        break;
+      }
+    } else if (attacker == player2) {
+      player1Health = attackPlayer(player1Health);
+      logHealth(player1, player1Health);
+      if (isDead(player1Health)) {
         logDeath(player2, player1);
+        break;
+      }
     }
-}
-}
-
-fight('Patrick Johnson', 'Alison Bourdon', 100, 100);
+  }
+};
+fight("Patrick", "Alison", 100, 100);
